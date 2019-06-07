904601127169f9e739e37eb3bc6a5edd91327d7f
xuanlanCognition <guoliangxuanlan@outlook.com>
2019-06-07 11:41:45 +0800

    Just a commit.

--- bat/syn_scan/syn_scan.py
+++ bat/syn_scan/syn_scan.py
@@ -12,11 +12,12 @@ class SynScanner(scan.Scanner):
 
     def p1(self):
         print("Please wait...")
-        ans, unans = sr(IP(dst=self.dnetwork)/TCP(dport=(1, 1000), flags='S'), timeout=10, verbose=0)
+        ans, unans = sr(IP(dst=self.dnetwork)/TCP(dport=(1, 1000), flags='S'), timeout=3, verbose=0)
         print("Bat report for " + self.dnetwork + '\n')
+        print(str(len(ans[ICMP])) +  " flitered ports")
         print("port\t\tstatus")
         ans.summary( lfilter =  lambda r: r[1].sprintf("%TCP.flags%") == 'SA', prn = lambda r: r[1].sprintf("%TCP.sport%\topen"))
-        ans.summary( lfilter =  lambda r: r[1].sprintf("%TCP.flags%") != 'SA', prn = lambda r: r[1].sprintf("%TCP.sport%\tclose"))
+        ans.summary( lfilter =  lambda r: r[1].sprintf("%TCP.flags%") == 'RA', prn = lambda r: r[1].sprintf("%TCP.sport%\tclose"))
 
     def p2(self):
         ans, unans = sr(IP(dst=self.dnetwork)/TCP(dport=int(self.dport), flags='S'), timeout=5, verbose=0)
